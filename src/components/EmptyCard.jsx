65e608a6f90d14359324baf4418fb702674cf8fb
jahid <jahid15-1499@diu.edu.bd>
2025-11-13 01:37:48 +0600

    create icon using svg

--- src/components/EmptyCard.jsx
+++ src/components/EmptyCard.jsx
@@ -1,3 +1,5 @@
+import NotFoundIcon from "../icon/NotFoundIcon";
+
 const EmptyCard = () => {
   return (
     <div
@@ -5,21 +7,7 @@ const EmptyCard = () => {
       data-sentry-component="EmptyList"
       data-sentry-source-file="EmptyList.tsx"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="lucide lucide-inbox h-12 w-12 text-muted-foreground"
-      >
-        <polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
-        <path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path>
-      </svg>
+      <NotFoundIcon />
       <div className="text-center">
         <p className="text-lg font-medium">Not Found</p>
         <p className="text-sm text-muted-foreground">Please add some credentials.</p>
--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,10 +1,10 @@
+import FilterIcon from "../icon/FilterIcon";
+
 const SearchInput = ({ handleSearch }) => {
   return (
     <label className="relative flex-1">
       <span className="pointer-events-none absolute left-4 top-1/2 -translate-y-1/2 text-neutral-500">
-        <svg className="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-        </svg>
+        <FilterIcon />
       </span>
       <input
         type="text"
